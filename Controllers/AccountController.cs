905cd17a08f6332aa2ce8c6a35e69625de687bb7
Ashutosh sharma <105362721+Gokuaf@users.noreply.github.com>
2023-08-04 14:35:09 +0530

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserApp.Models.VM;
+
+namespace UserApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost] 
+        public ActionResult Register(VMRegister register) 
+        {
+            return View();
+         }
+    
+    
+    }
+
+}
---
+++ Models/VM/VMRegister.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UserApp.Models.VM
+{
+    public class VMRegister
+    {
+        public Guid ID { get; set; }
+
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+       
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Enter Valid E-mail")]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string RePassword { get; set; }
+        public string CreatedOn { get; set; }
+        public string LastLogin { get; set; }
+}
+}
